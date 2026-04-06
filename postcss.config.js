d803a784d630c230c40a379aeff295f43dc30322
Felipe Munevar N <felipemunevarn@gmail.com>
2026-04-06 15:50:35 -0300

    Initialization application

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: {
+    "@tailwindcss/postcss": {}, // Note the new name here
+    autoprefixer: {},
+  },
+}
--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import LoginForm from './components/LoginForm';
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <LoginForm />
     </div>
   );
 }
